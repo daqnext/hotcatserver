115a1886aa686ecc987e780ae9eeac96a6bd96a4
goodboy3 <zhangzhengbo@hotmail.com>
2021-07-25 15:24:05 +0800

    commit

--- src/config/conf.ts
+++ src/config/conf.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-07-07 10:47:59
- * @LastEditTime: 2021-07-09 09:03:55
+ * @LastEditTime: 2021-07-25 15:09:18
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /hotcatserver/src/config/conf.ts
@@ -13,8 +13,8 @@ if(process.argv.includes("dev_region_config")){
     config=require('./dev_region_config');
 }
 
-if(process.argv.includes("prod_config")){
-    config=require('./prod_config');
+if(process.argv.includes("prod_center_config")){
+    config=require('./prod_center_config');
 }
 
 export  {config};
